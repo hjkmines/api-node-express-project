eaa94fe30236028e34a3cd4e67e66adc4594a94d
Tony Kim <hjkmines@gmail.com>
2020-08-07 16:09:06 -0600

    adds route

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ dotenv.config({path: './config/config.env'});
 
 const app = express(); 
 
+//routes 
+app.get('/', (req, res) => {
+  res.status(200).json({success: true, data: {id: 5}}); 
+})
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server is running on port ${process.env.NODE_ENV} mode on port ${PORT}`))
+
